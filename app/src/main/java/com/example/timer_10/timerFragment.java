986b44fdd2e7b583fcd0796917a59f8a790d3e7e
calr1 <43725535+calr1@users.noreply.github.com>
2021-04-29 18:05:14 +0100

    teste

--- app/src/main/java/com/example/timer_10/timerFragment.java
+++ app/src/main/java/com/example/timer_10/timerFragment.java
@@ -18,7 +18,6 @@ import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
-
 import static android.app.Activity.RESULT_OK;
 
 public class timerFragment extends Fragment {
@@ -27,7 +26,6 @@ public class timerFragment extends Fragment {
     private TextView hoursValue, minutesValue, secondsValue;
     private Timer timer;
     private ConstraintLayout layout;
-
     private boolean timerRunning, timerCreated;
     private long timerInitialValue;
     private int timerCountdownInterval;
@@ -37,10 +35,7 @@ public class timerFragment extends Fragment {
     private EditText et;
     private CheckBox cb;
 
-    private static final int mediaTimer = 1;
-    private static final int configure = 2;
-
-    private long time, lastValue;
+    private static final int configure = 1;
 
 
     @Override
@@ -76,7 +71,6 @@ public class timerFragment extends Fragment {
         et = getView().findViewById(R.id.inputIntervals);
         cb = getView().findViewById(R.id.notificationsCheckBox);
 
-
         playPauseButton.setOnClickListener(v -> {
             if (!timerCreated) {
                 startStopTimer();
@@ -85,9 +79,7 @@ public class timerFragment extends Fragment {
                 Log.d("TIMER initial value: ", String.valueOf(timerInitialValue));
                 timer.setTimerCountdownInterval(timerCountdownInterval);
                 timer.createTimer(timerInitialValue, timerCountdownInterval);
-
                 timer.startTimer();
-                timer.setTimerName(name);
                 Toast.makeText(getActivity(), "Timer started!", Toast.LENGTH_SHORT).show();
                 playPauseButton.setImageResource(R.drawable.ic_baseline_pause_24);
 
@@ -102,7 +94,6 @@ public class timerFragment extends Fragment {
             intent.putExtra("timerIndex", wrapper.getIndexOfIndividualTimer(timer));
             startActivity(intent);
 
-
         });
 
         layout = getView().findViewById(R.id.frameLayout);
@@ -111,7 +102,6 @@ public class timerFragment extends Fragment {
             intent.putExtra("timerIndex", wrapper.getIndexOfIndividualTimer(timer));
             startActivityForResult(intent, configure);
         });
-
 
     }
 
@@ -137,7 +127,6 @@ public class timerFragment extends Fragment {
     }
 
 
-
     private void stopAlarm() {
         timer.stopSound();
     }
@@ -152,54 +141,4 @@ public class timerFragment extends Fragment {
             }
         }
     }
-
-                assert data != null;
-                assert data != null;
-                intervals = data.getBooleanExtra("intervals", false);
-                numberOfIntervals = data.getIntExtra("numberOfIntervals", 1);
-                mode = data.getStringExtra("mode");
-                name = data.getStringExtra("name");
-                time = data.getLongExtra("time", 0);
-                if (timer != null) {
-                    timer.setMode(mode);
-                    updateTimer(time);
-                }
-            }
-        }
-        if (requestCode == mediaTimer) {
-            if (resultCode == RESULT_OK) {
-                assert data != null;
-                intervals = data.getBooleanExtra("intervals", false);
-                numberOfIntervals = data.getIntExtra("numberOfIntervals", 1);
-                mode = data.getStringExtra("mode");
-                name = data.getStringExtra("name");
-                time = data.getLongExtra("time", 0);
-                updateTimer(time);
-                if (timer != null) {
-                    timer.setMode(mode);
-                    //updateTimer(time);
-                }
-                else{
-
-                }
-            }
-        }
-    }
-
-    public void startStopTimer() {
-        hoursValue.setEnabled(timerCreated);
-        minutesValue.setEnabled(timerCreated);
-        secondsValue.setEnabled(timerCreated);
-        timerRunning = !timerCreated;
-        timerCreated = !timerCreated;
-    }
-
-    private void stopAlarm() {
-        timer.stopSound();
-    }
-
-    public void setLastValue(long lastValue){
-        this.lastValue = lastValue;
-    }
-
 }
